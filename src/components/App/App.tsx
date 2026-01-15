6234a481cf345a393f6ed76f3a62dfb529e8473b
OlenaKarpova <kal4enko@gmail.com>
2026-01-15 12:28:12 +0100

    react hv2 v 1.0

---
+++ src/components/App/App.tsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import type { Votes, VoteType } from '../../types/votes';
+import css from './App.module.css';
+
+import CafeInfo from '../CafeInfo/CafeInfo';
+import VoteOptions from '../VoteOptions/VoteOptions';
+import VoteStats from '../VoteStats/Votestats';
+import Notification from '../Notification/Notification';
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({good: 0, neutral: 0, bad: 0});
+
+  const handleVote = (voteName: keyof Votes) => {
+    setVotes({ ...votes, [voteName]: votes[voteName] + 1 });
+  }
+  const resetVotes = () => {
+    setVotes({good: 0, neutral: 0, bad: 0});
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+  ? Math.round((votes.good / totalVotes) * 100)
+  : 0;
+
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+
+      {totalVotes > 0 ? (
+        <VoteStats
+          votes={votes}
+          totalVotes = { totalVotes }
+          positiveRate={positiveRate}
+        />
+      ) : (
+          <Notification/>
+      )}
+    </div>
+  );
+}
