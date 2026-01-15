504a01aa21a1a55a8bc79eb302e5244f2518e3ca
OlenaKarpova <kal4enko@gmail.com>
2026-01-15 12:42:45 +0100

    react hw2 v 1.2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import type { Votes, VoteType } from '../../types/votes';
+import type { Votes } from '../../types/votes';
 import css from './App.module.css';
 
 import CafeInfo from '../CafeInfo/CafeInfo';
