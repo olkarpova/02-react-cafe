c93e86d33de309e6480672362986e47a55a09d36
OlenaKarpova <kal4enko@gmail.com>
2026-01-15 14:01:28 +0100

    renamed VoteStats

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import css from './App.module.css';
 
 import CafeInfo from '../CafeInfo/CafeInfo';
 import VoteOptions from '../VoteOptions/VoteOptions';
-import VoteStats from '../VoteStats/Votestats';
+import VoteStats from '../VoteStats/VoteStats';
 import Notification from '../Notification/Notification';
 
 export default function App() {
